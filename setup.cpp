ee18e1ffbd870dd1f394ed4b6fdf891978896acb
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2016-04-02 22:11:37 +0900

    add setup program

---
+++ setup.cpp
@@ -0,0 +1,99 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <cstring>
+
+INT DoCopyFiles(VOID) {
+  WCHAR szPathSrc[MAX_PATH], szPathDest[MAX_PATH], *pch;
+
+  // source
+  GetModuleFileName(NULL, szPathSrc, MAX_PATH);
+  pch = wcsrchr(szPathSrc, L'\\');
+  lstrcpy(pch, L"\\mzimeja.dic");
+  // dest
+  GetWindowsDirectory(szPathDest, MAX_PATH);
+  wcscat(szPathDest, L"\\mzimeja.dic");
+  // copy
+  BOOL b0 = CopyFile(szPathSrc, szPathDest, FALSE);
+  if (!b0) {
+    return 1;
+  }
+
+  // source
+  GetModuleFileName(NULL, szPathSrc, MAX_PATH);
+  pch = wcsrchr(szPathSrc, L'\\');
+  lstrcpy(pch, L"\\mzimeja.ime");
+  // dest
+  GetSystemDirectory(szPathDest, MAX_PATH);
+  wcscat(szPathDest, L"\\mzimeja.ime");
+  // copy
+  BOOL b1 = CopyFile(szPathSrc, szPathDest, FALSE);
+  if (!b1) {
+    return 2;
+  }
+
+  return 0;
+}
+
+BOOL DoSetRegSz(HKEY hKey, LPCWSTR pszName, LPCWSTR pszValue) {
+  DWORD cbData = (lstrlenW(pszValue) + 1) * sizeof(WCHAR);
+  LONG result;
+  result = RegSetValueExW(hKey, pszName, 0, REG_SZ, (LPBYTE)pszValue, cbData);
+  return result == ERROR_SUCCESS;
+}
+
+INT DoSetRegistry(VOID) {
+  BOOL ret = FALSE;
+  HKEY hKey;
+  LONG result = RegOpenKeyExW(HKEY_LOCAL_MACHINE, 
+    L"system\\currentcontrolset\\control\\keyboard layouts",
+    0, KEY_WRITE, &hKey);
+  if (result == ERROR_SUCCESS && hKey) {
+    HKEY hkLayouts;
+    DWORD dwDisposition;
+    result = RegCreateKeyExW(hKey, L"E0120411", 0, NULL, 0,
+                             KEY_WRITE, NULL, &hkLayouts, &dwDisposition);
+    if (result == ERROR_SUCCESS && hkLayouts) {
+      if (DoSetRegSz(hkLayouts, L"layout file", L"kbdjp.kbd") &&
+        DoSetRegSz(hkLayouts, L"layout text", L"日本語 (MZ-IME)") &&
+        DoSetRegSz(hkLayouts, L"layout file", L"kbdjp.kbd") &&
+        DoSetRegSz(hkLayouts, L"IME file", L"mzimeja.ime"))
+      {
+        
+        ret = TRUE;
+      }
+      RegCloseKey(hkLayouts);
+    }
+    RegCloseKey(hKey);
+  }
+  return (ret ? 0 : -1);
+}
+
+extern "C"
+INT WINAPI
+wWinMain(
+  HINSTANCE hInstance,
+  HINSTANCE HPrevInstance,
+  LPTSTR    lpCmdLine,
+  INT       nCmdShow)
+{
+  if (0 != DoSetRegistry()) {
+    MessageBox(NULL,
+      L"レジストリの設定に失敗しました。管理者権限で実行して下さい。",
+      NULL, MB_ICONERROR);
+    return 2;
+  }
+
+  if (0 != DoCopyFiles()) {
+    MessageBox(NULL,
+      L"ファイルのコピーに失敗しました。管理者権限で実行して下さい。",
+      NULL, MB_ICONERROR);
+    return 1;
+  }
+
+  WCHAR szPath[MAX_PATH];
+  GetSystemDirectory(szPath, MAX_PATH);
+  wcscat(szPath, L"\\mzimeja.ime");
+  ImmInstallIME(szPath, TEXT("日本語 (MZ-IME)"));
+
+  return 0;
+}
