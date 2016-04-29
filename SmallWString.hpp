2e8fad2bdb6729e575426cb2050b434efc232d1b
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2016-04-29 13:25:28 +0900

    fix SmallWString

--- SmallWString.hpp
+++ SmallWString.hpp
@@ -7,65 +7,72 @@ class SmallWString {
 public:
     SmallWString() { m_buf[0] = 0; }
     SmallWString(const wchar_t *str) {
-        *this = str;
+        assign(str);
     }
     SmallWString(const wchar_t *str, size_t len) {
         assign(str, len);
     }
     SmallWString(const SmallWString& str) {
-        using namespace std;
-        memcpy(m_buf, str.m_buf, c_capacity * sizeof(wchar_t));
+        assign(str);
     }
 
     bool empty() const { return size() == 0; }
     size_t size() const {
-        for (size_t i = 0; i < capacity; ++i) {
+        for (size_t i = 0; i < c_capacity; ++i) {
             if (m_buf[i] == 0) return i;
         }
-        return capacity;
+        return c_capacity;
     }
 
     SmallWString& operator=(const wchar_t *str) {
         using namespace std;
         size_t len = wcslen(str);
-        if (len > capacity) len = capacity;
+        if (len > c_capacity) len = c_capacity;
         memcpy(m_buf, str, len * sizeof(wchar_t));
-        if (len < capacity) {
+        if (len < c_capacity) {
             m_buf[len] = 0;
         }
         return *this;
     }
+
+    SmallWString& assign(const wchar_t *str) {
+        return assign(str, wcslen(str));
+    }
+    SmallWString& assign(const SmallWString& str) {
+        using namespace std;
+        memcpy(m_buf, str.m_buf, c_capacity * sizeof(wchar_t));
+    }
     SmallWString& assign(size_t count, wchar_t ch) {
-        if (count > capacity) {
-            count = capacity;
+        if (count > c_capacity) {
+            count = c_capacity;
         }
         for (size_t i = 0; i < count; ++i) {
             m_buf[i] = ch;
         }
-        if (count < capacity) {
+        if (count < c_capacity) {
             m_buf[count] = 0;
         }
         return *this;
     }
     SmallWString& assign(const wchar_t *str, size_t len) {
-        if (len > capacity) {
-            len = capacity;
+        if (len > c_capacity) {
+            len = c_capacity;
         }
         for (size_t i = 0; i < len; ++i) {
             m_buf[i] = str[i];
         }
-        if (len < capacity) {
+        if (len < c_capacity) {
             m_buf[len] = 0;
         }
         return *this;
     }
 
     wchar_t& operator[](size_t index) {
-        assert(index < capacity);
+        assert(index < c_capacity);
         return m_buf[index];
     }
     const wchar_t& operator[](size_t index) const {
-        assert(index < capacity);
+        assert(index < c_capacity);
         return m_buf[index];
     }
     const wchar_t *c_str() const { return m_buf; }
@@ -73,7 +80,7 @@ public:
 
 protected:
     static const size_t c_capacity = 6;
-    wchar_t m_buf[capacity];
+    wchar_t m_buf[c_capacity];
 }; // class SmallWString
 
 #endif  // ndef SMALLWSTRING_HPP_
