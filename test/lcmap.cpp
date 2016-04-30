90da68c21511b3b466f933c36c68b92292063dd2
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2016-04-30 14:14:48 +0900

    add test/lcmap.cpp

---
+++ test/lcmap.cpp
@@ -0,0 +1,60 @@
+// lcmap.cpp
+// (Japanese, Shift_JIS)
+#include <windows.h>
+#include <string>
+#include <iostream>
+
+std::string lcmap(const std::string& str, DWORD dwFlags) {
+  CHAR szBuf[1024];
+  const LCID langid = MAKELANGID(LANG_JAPANESE, SUBLANG_DEFAULT);
+  ::LCMapStringA(MAKELCID(langid, SORT_DEFAULT), dwFlags,
+    str.c_str(), -1, szBuf, 1024);
+  return std::string(szBuf);
+}
+
+// The result is:
+// 
+//   漢字あいうあいうｱｲｳＡＢＣABCabc。
+//   漢字アイウアイウｱｲｳＡＢＣABCabc。
+//   漢字あいうあいうあいうＡＢＣＡＢＣａｂｃ。
+//   漢字アイウアイウアイウＡＢＣＡＢＣａｂｃ。
+//   漢字あいうアイウアイウＡＢＣＡＢＣａｂｃ。
+//   漢字ｱｲｳｱｲｳｱｲｳABCABCabc｡
+//   漢字あいうｱｲｳｱｲｳABCABCabc｡
+
+int main(void) {
+  std::string str, strMapped;
+  DWORD dwFlags;
+
+  str = "漢字あいうアイウｱｲｳＡＢＣABCabc。";
+
+  dwFlags = LCMAP_HIRAGANA;
+  strMapped = lcmap(str, dwFlags);
+  std::cout << strMapped << std::endl;
+
+  dwFlags = LCMAP_KATAKANA;
+  strMapped = lcmap(str, dwFlags);
+  std::cout << strMapped << std::endl;
+
+  dwFlags = LCMAP_FULLWIDTH | LCMAP_HIRAGANA;
+  strMapped = lcmap(str, dwFlags);
+  std::cout << strMapped << std::endl;
+
+  dwFlags = LCMAP_FULLWIDTH | LCMAP_KATAKANA;
+  strMapped = lcmap(str, dwFlags);
+  std::cout << strMapped << std::endl;
+
+  dwFlags = LCMAP_FULLWIDTH;
+  strMapped = lcmap(str, dwFlags);
+  std::cout << strMapped << std::endl;
+
+  dwFlags = LCMAP_HALFWIDTH | LCMAP_KATAKANA;
+  strMapped = lcmap(str, dwFlags);
+  std::cout << strMapped << std::endl;
+
+  dwFlags = LCMAP_HALFWIDTH;
+  strMapped = lcmap(str, dwFlags);
+  std::cout << strMapped << std::endl;
+
+  return 0;
+}
