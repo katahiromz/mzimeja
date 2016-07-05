fe761f941c33713c9e10dff6d1fb867847a55ca6
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2016-07-05 20:37:16 +0900

    trivial 3

--- setup.cpp
+++ setup.cpp
@@ -59,7 +59,7 @@ INT DoSetRegistry(VOID) {
   LONG result = RegOpenKeyExW(HKEY_LOCAL_MACHINE, 
     L"system\\currentcontrolset\\control\\keyboard layouts",
     0, KEY_WRITE, &hKey);
-  if (result == ERROR_SUCCESS && hKey != NULL) {
+  if (result == ERROR_SUCCESS && hKey) {
     HKEY hkLayouts;
     DWORD dwDisposition;
     result = RegCreateKeyExW(hKey, L"E0120411", 0, NULL, 0,
