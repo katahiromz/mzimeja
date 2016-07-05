94c8d4a0d1a302176bb9ed4838d79b6d0c9569bd
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2016-07-05 20:32:07 +0900

    update unboost.hpp

--- unboost.hpp
+++ unboost.hpp
@@ -3,7 +3,7 @@
 //////////////////////////////////////////////////////////////////////////////
 
 #ifndef UNBOOST_HPP_
-#define UNBOOST_HPP_    9 // Version 9
+#define UNBOOST_HPP_    10 // Version 10
 
 #ifndef __cplusplus
     #error Unboost needs C++ compiler. You lose.
@@ -89,12 +89,35 @@
 // static_assert
 
 #ifdef UNBOOST_USE_STATIC_ASSERT
-    #ifndef UNBOOST_CXX11   // not C++11
-        // Use Boost
+    // If not choosed, choose one
+    #if ((defined(UNBOOST_USE_CXX11_STATIC_ASSERT) + defined(UNBOOST_USE_UNBOOST_STATIC_ASSERT) + defined(UNBOOST_USE_BOOST_STATIC_ASSERT)) == 0)
+        #ifdef UNBOOST_USE_CXX11
+            #define UNBOOST_USE_CXX11_STATIC_ASSERT
+        #elif defined(UNBOOST_USE_BOOST)
+            #define UNBOOST_USE_BOOST_STATIC_ASSERT
+        #else
+            #ifdef UNBOOST_CXX11   // C++11
+                #define UNBOOST_USE_CXX11_STATIC_ASSERT
+            #else
+                #define UNBOOST_USE_UNBOOST_STATIC_ASSERT
+            #endif
+        #endif
+    #endif
+    // Adapt choosed one
+    #ifdef UNBOOST_USE_CXX11_STATIC_ASSERT
+        // static_assert is available
+        #define UNBOOST_STATIC_ASSERT_MSG static_assert
+    #elif defined(UNBOOST_USE_BOOST_STATIC_ASSERT)
+        // Use Boost BOOST_STATIC_ASSERT_MSG
         #include <boost/static_assert.hpp>
+        #define UNBOOST_STATIC_ASSERT_MSG BOOST_STATIC_ASSERT_MSG
         #ifndef static_assert
             #define static_assert BOOST_STATIC_ASSERT_MSG
         #endif
+    #else
+        #define UNBOOST_STATIC_ASSERT_MSG(x,y) \
+            typedef UNBOOST_STATIC_ASSERTION[(x) ? 1 : -1]
+        #define static_assert(x,y) UNBOOST_STATIC_ASSERT_MSG((x),(y))
     #endif
 #endif
 
