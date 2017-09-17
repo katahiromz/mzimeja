0e6b3cf2c67f9d7a9aec9320829628cb82b12ed8
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2017-09-17 10:16:39 +0900

    fix make_dict.bat

--- make_dict.bat
+++ make_dict.bat
@@ -1,9 +1,16 @@
-if exist Release\dict_compile.exe goto label2
+if exist build\dict_compile.exe goto label1
+if exist build\Debug\dict_compile.exe goto label2
+if exist build\Release\dict_compile.exe goto label2
 :label1
-Debug\dict_compile.exe ..\res\mzimeja.dat ..\res\mzimeja.dic
-Debug\dict_compile.exe ..\res\name.dat ..\res\name.dic
-goto label3
+build\dict_compile.exe res\mzimeja.dat res\mzimeja.dic
+build\dict_compile.exe res\name.dat res\name.dic
+goto label4
 :label2
-Release\dict_compile.exe ..\res\mzimeja.dat ..\res\mzimeja.dic
-Release\dict_compile.exe ..\res\name.dat ..\res\name.dic
+build\Debug\dict_compile.exe res\mzimeja.dat res\mzimeja.dic
+build\Debug\dict_compile.exe res\name.dat res\name.dic
+goto label4
 :label3
+build\Release\dict_compile.exe res\mzimeja.dat res\mzimeja.dic
+build\Release\dict_compile.exe res\name.dat res\name.dic
+goto label4
+:label4
