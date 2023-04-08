d2d4a34afcfb3dc62760a390c4446b94533d4b5a
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-04-08 09:52:17 +0900

    improve postal.cpp

--- ime/postal.cpp
+++ ime/postal.cpp
@@ -1,59 +1,67 @@
 // 郵便番号変換。
 #include "mzimeja.h"
 
+// 郵便番号変換を行う関数。
 std::wstring postal_code(LPCWSTR code)
 {
+    std::wstring postal, ret;
+    if (Config_GetDWORD(L"PostalDictDisabled", FALSE)) // 無効化されている？
+        return ret;
+
+    if (!Config_GetSz(L"PostalDictPathName", postal)) // 郵便番号データのパス名を取得できない？
+        return ret;
+
     DWORD dwTick1 = ::GetTickCount(); // 測定開始。
 
-    // TODO: ここのコードをなるべく高速にせよ。
-    std::wstring postal, ret;
-    if (!Config_GetDWORD(L"PostalDictDisabled", FALSE)) {
-        if (Config_GetSz(L"PostalDictPathName", postal)) {
-            // 郵便番号データのファイルを開く。
-            if (FILE *fin = _wfopen(postal.c_str(), L"rb")) {
-                // 一行ずつ処理する。
-                char buf[256];
-                while (fgets(buf, _countof(buf), fin)) {
-                    // 前後の空白を除去。
-                    StrTrimA(buf, " \r\n");
+    CHAR szCodeA[16];
+    StringCchPrintfA(szCodeA, _countof(szCodeA), "%07u", code);
 
-                    // コメントの除去。
-                    if (LPSTR pch = strchr(buf, ';')) {
-                        *pch = 0;
-                        StrTrimA(buf, " \r\n");
-                    }
+    // 郵便番号データのファイルを開く。
+    if (FILE *fin = _wfopen(postal.c_str(), L"rb")) {
+        // 一行ずつ処理する。
+        char szLine[MAX_PATH];
+        while (fgets(szLine, _countof(szLine), fin)) {
+            // タブ文字以外の前後の空白を除去。
+            StrTrimA(szLine, " \r\n");
 
-                    // UTF-8からUTF-16へ変換。
-                    WCHAR szLine[256];
-                    ::MultiByteToWideChar(CP_UTF8, 0, buf, -1, szLine, _countof(szLine));
-                    szLine[_countof(szLine) - 1] = 0; // Avoid buffer overflow
-
-                    // タブ文字で分割。
-                    std::wstring line = szLine;
-                    std::vector<std::wstring> fields;
-                    str_split(fields, line, std::wstring(L"\t"));
-
-                    // フィールドが2個以下なら失敗。
-                    if (fields.size() < 2)
-                        continue;
-
-                    // 第一フィールドがcodeでなければ失敗。
-                    if (fields[0] != code)
-                        continue;
-
-                    // 発見。
-                    ret = fields[1];
-                    break;
-                }
-
-                // ファイルを閉じる。
-                fclose(fin);
+            // コメントの除去。
+            if (LPSTR pch1 = strchr(szLine, ';')) {
+                *pch1 = 0;
+                StrTrimA(szLine, " \r\n");
             }
+
+            // タブ文字で分割。
+            LPSTR pch2 = strchr(szLine, '\t');
+            if (!pch2)
+                continue; // タブがなければ次の行。
+            *pch2++ = 0;
+
+            // 各フィールドの前後の空白を除去。
+            StrTrimA(szLine, " \t\r\n");
+            StrTrimA(pch2, " \t\r\n");
+
+            // 一致しなければ次の行。
+            if (lstrcmpA(szCodeA, szLine) != 0)
+                continue;
+
+            // UTF-8からUTF-16へ変換。
+            WCHAR szValue[MAX_PATH];
+            ::MultiByteToWideChar(CP_UTF8, 0, pch2, -1, szValue, _countof(szValue));
+            szValue[_countof(szValue) - 1] = 0; // Avoid buffer overflow
+
+            // 結果をセット。
+            ret = szValue;
+            break;
         }
+
+        // ファイルを閉じる。
+        fclose(fin);
     }
 
     DWORD dwTick2 = ::GetTickCount(); // 測定終了。
 
+    // 測定値をデバッグ出力。
     DPRINT("postal_code: %lu\n", dwTick2 - dwTick1);
+
     return ret;
 }
