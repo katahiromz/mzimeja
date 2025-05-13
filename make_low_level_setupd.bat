97665718905a7b648a40ef825f301e1211e072a3
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-05-13 16:11:25 +0900

    fix debug installer name

--- make_low_level_setupd.bat
+++ make_low_level_setupd.bat
@@ -1,6 +1,6 @@
 set VERSION=0.9.9.1
-set DEST_DIR=mzimejad-%VERSION%-low-level-setup
-set OUTPUT=mzimejad-%VERSION%-low-level-setup.exe
+set DEST_DIR=mzimeja-%VERSION%d-low-level-setup
+set OUTPUT=mzimeja-%VERSION%d-low-level-setup.exe
 if not exist "%DEST_DIR%" mkdir "%DEST_DIR%"
 if exist archive.7z del archive.7z
 for %%F in (READMEJP.txt LICENSE.txt HISTORY.txt res\basic.dic res\name.dic res\kanji.dat res\radical.dat res\postal.dat build\Debug\mzimeja.ime build\Debug\ime_setup.exe build\Debug\imepad.exe build\Debug\dict_compile.exe build\Debug\verinfo.exe) do copy %%F "%DEST_DIR%"
